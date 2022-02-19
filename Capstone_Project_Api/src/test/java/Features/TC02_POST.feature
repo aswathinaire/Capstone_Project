9a3a0e9fbcdc2eeb75fc1aa7032b98f452167fea
aswathi.nair <aswathi.nair@inbl01lap7116.brd.g-c-i.com>
2022-02-19 18:35:22 +0530

    Capstone_Project_Api

---
+++ Capstone_Project_Api/src/test/java/Features/TC02_POST.feature
@@ -0,0 +1,9 @@
+@UserInfoAPI2 @APISuite @Post
+Feature: User_Info_APIPOst
+
+Scenario: User Information verification with valid details
+Given I have valid URL for post request
+Then I should receive status code as 201
+And Verify the id generated is correct
+    
+
---
+++ Capstone_Project_Api/src/test/java/Runners/TestRunner.java
@@ -0,0 +1,18 @@
+package Runners; 
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/Features/"},
+        glue = {"Steps"},
+        //tags = {"@APISuite"}
+        tags = {"@GET"}
+        //tags = {"@Post"}
+        //tags = {"@put"}
+        //tags = {"@Delete"}
+       )
+public class  TestRunner{
+}
